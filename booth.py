825d7d8fee0ebf7faddc6c9927b33a582b7f64e2
Qais Patankar <qaisjp@gmail.com>
2018-11-10 16:15:31 +0000

    fix typo

--- booth.py
+++ booth.py
@@ -38,7 +38,7 @@ def takePhotos():
     w, h = Image.open(img_names[0]).size
     out = Image.new("RGB", (2 * w + 30, 2 * h + 30), "white")
 
-    shuffle(images)
+    shuffle(img_names)
     out.paste(Image.open(img_names[0]), (10, 10))
     out.paste(Image.open(img_names[1]), (w + 20, 10))
     out.paste(Image.open(img_names[2]), (10, h + 20))
